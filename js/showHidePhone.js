6eaa0bb72aed225d13d3bc7ff354137d8e3bbc2c
al-badarin <jamalalbadarin@gmail.com>
2024-06-27 20:26:22 +0300

    Update showHidePhone.js

--- js/showHidePhone.js
+++ js/showHidePhone.js
@@ -1,25 +1,23 @@
 function showHidePhoneNumber() {
-    const phoneNumberElement = document.getElementById('phoneNumber');
-    phoneNumberElement.addEventListener('click', () => {
-        if (!phoneNumberElement.textContent.startsWith('+')) {
-            phoneNumberElement.textContent = '+359 898 75 00 67';
-            phoneNumberElement.style.cursor = 'default';
-            copyButton.removeAttribute('hidden');
-        }
-    })
+  const phoneNumberElement = document.getElementById("phoneNumber");
+  phoneNumberElement.addEventListener("click", () => {
+    if (!phoneNumberElement.textContent.startsWith("+")) {
+      phoneNumberElement.textContent = "+359 888881476";
+      phoneNumberElement.style.cursor = "default";
+      copyButton.removeAttribute("hidden");
+    }
+  });
 
-    const copyButton = document.createElement('button');
-    copyButton.textContent = 'Copy';
-    copyButton.setAttribute('hidden', 'true');
-    copyButton.addEventListener('click', () => {
-        navigator.clipboard.writeText(phoneNumberElement.textContent);
-        copyButton.textContent = 'Copied!';
-        copyButton.disabled = true;
-    })
+  const copyButton = document.createElement("button");
+  copyButton.textContent = "Copy";
+  copyButton.setAttribute("hidden", "true");
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(phoneNumberElement.textContent);
+    copyButton.textContent = "Copied!";
+    copyButton.disabled = true;
+  });
 
-    phoneNumberElement.after(copyButton);
-
+  phoneNumberElement.after(copyButton);
 }
 
-
-showHidePhoneNumber()
+showHidePhoneNumber();
